38f62b653c0051b457c3607a22a74d8aad6b7168
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-05-16 00:40:16 +1000

    V2.0.1

--- middleware.js
+++ middleware.js
@@ -1,5 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-export default clerkMiddleware();
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  ignoredRoutes: ["/api/webhooks(.*)"],
+});
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
