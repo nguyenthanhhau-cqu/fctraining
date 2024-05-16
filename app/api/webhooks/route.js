f7ebf7ad695ed46e0cbe8bef2fe2f783aa769d97
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-05-16 17:08:42 +1000

    V2.0.1

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -2,41 +2,33 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { createOrUpdateUser, deleteUser } from "@lib/actions/user";
 
-
-
 export async function POST(req) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhooks
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
+    console.error("WEBHOOK_SECRET is not set");
     throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+        "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
   }
 
-  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
-  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+    console.error("Missing Svix headers");
+    return new Response("Error occurred -- no svix headers", {
       status: 400,
     });
   }
 
-  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
-
-  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
-
   let evt;
 
-  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -50,22 +42,13 @@ export async function POST(req) {
     });
   }
 
-  // Handle the event
   const eventType = evt?.type;
 
   if (eventType === "user.created" || eventType === "user.updated") {
-    const { id, first_name, last_name, image_url, email_addresses, username } =
-      evt?.data;
+    const { id, first_name, last_name, image_url, email_addresses } = evt?.data;
 
     try {
-      await createOrUpdateUser(
-        id,
-        first_name,
-        last_name,
-        image_url,
-        email_addresses,
-      );
-
+      await createOrUpdateUser(id, first_name, last_name, image_url, email_addresses);
       return new Response("User is created or updated", {
         status: 200,
       });
@@ -81,7 +64,6 @@ export async function POST(req) {
     try {
       const { id } = evt?.data;
       await deleteUser(id);
-
       return new Response("User is deleted", {
         status: 200,
       });
@@ -92,4 +74,9 @@ export async function POST(req) {
       });
     }
   }
+
+  console.error("Unhandled event type:", eventType);
+  return new Response("Unhandled event type", {
+    status: 400,
+  });
 }
