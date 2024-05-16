7f6aa763cf3ebe6576fb1afe1288884579902477
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-05-16 16:54:16 +1000

    V2.0.1

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 import { clerkMiddleware } from "@clerk/nextjs/server"
 export default clerkMiddleware()
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)", "/api/webhook(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)", "/api/webhooks"],
 }
