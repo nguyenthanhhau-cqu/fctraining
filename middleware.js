9c6d892da833f05e7e42242fefe73d039598bd34
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-05-17 13:44:39 +1000

    V2.0.1

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware } from "@clerk/nextjs/server"
 
 export default clerkMiddleware({
-  apiUrl: "/api/webhook",
+  apiUrl: "/api/webhook/clerk",
 });
 
 export const config = {
