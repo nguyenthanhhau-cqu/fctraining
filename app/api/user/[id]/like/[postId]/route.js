732c1d6843ba86bca686ff58b27b68efa816b3b7
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-05-29 18:56:10 +1000

    V3

--- app/api/user/[id]/like/[postId]/route.js
+++ app/api/user/[id]/like/[postId]/route.js
@@ -1,41 +1,53 @@
-import Post from "@lib/models/Post"
-import User from "@lib/models/User"
-import { connectToDatabase } from "@lib/mongodb/mongoose"
+import Post from "@lib/models/Post";
+import User from "@lib/models/User";
+import { connectToDatabase } from "@lib/mongodb/mongoose";
 
 export const POST = async (req, { params }) => {
   try {
-    await connectToDatabase()
+    await connectToDatabase();
 
-    const userId = params.id
-    const postId = params.postId
+    const userId = params.id;
+    const postId = params.postId;
 
-    const user = await User.findOne({ clerkId: userId }).populate("posts savedPosts following followers").populate({
-      path: "likedPosts",
-      model: "Post",
-      populate: {
-        path: "creator",
-        model: "User",
-      },
-    })
+    const user = await User.findOne({ clerkId: userId })
+        .populate("posts savedPosts following followers")
+        .populate({
+          path: "likedPosts",
+          model: "Post",
+          populate: {
+            path: "creator",
+            model: "User",
+          },
+        });
 
-    const post = await Post.findById(postId).populate("creator likes")
-
-    const isLiked = user.likedPosts.find((item) => item._id.toString() === postId)
-
-    if (isLiked) {
-      user.likedPosts = user.likedPosts.filter((item) => item._id.toString() !== postId)
-      post.likes = post.likes.filter((item) => item._id.toString() !== user._id.toString())
-    } else {
-      user.likedPosts.push(post._id)
-      post.likes.push(user._id)
+    if (!user) {
+      console.error("User not found");
+      return new Response("User not found", { status: 404 });
     }
 
-    await user.save()
-    await post.save()
+    const post = await Post.findById(postId).populate("creator likes");
 
-    return new Response(JSON.stringify(user), { status: 200 })
+    if (!post) {
+      console.error("Post not found");
+      return new Response("Post not found", { status: 404 });
+    }
+
+    const isLiked = user.likedPosts.find((item) => item._id.toString() === postId);
+
+    if (isLiked) {
+      user.likedPosts = user.likedPosts.filter((item) => item._id.toString() !== postId);
+      post.likes = post.likes.filter((item) => item._id.toString() !== user._id.toString());
+    } else {
+      user.likedPosts.push(post._id);
+      post.likes.push(user._id);
+    }
+
+    await user.save();
+    await post.save();
+
+    return new Response(JSON.stringify(user), { status: 200 });
   } catch (err) {
-    console.log(err)
-    return new Response("Failed to like/dislike post", { status: 500 })
+    console.error("Error liking/disliking post:", err);
+    return new Response("Failed to like/dislike post", { status: 500 });
   }
-}
+};
