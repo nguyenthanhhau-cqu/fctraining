2770f332d46a7f80102f5ecd3c1622bf1f8a4049
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-06-01 02:18:17 +1000

    V3

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,44 +1,50 @@
-"use client";
+'use client';
 
 import { useUser } from "@clerk/nextjs";
 import Loader from "@components/Loader";
 import PostCard from "@components/cards/PostCard";
-import { useEffect, useState } from "react";
+import useSWR from 'swr';
+import { useEffect } from "react";
+import { useRouter } from 'next/navigation';
+
+// Fetcher function to fetch data from the API
+const fetcher = url => fetch(url).then(res => res.json());
 
 const Home = () => {
     const { user, isLoaded } = useUser();
+    const router = useRouter();
 
-    const [loading, setLoading] = useState(true);
-
-    const [feedPost, setFeedPost] = useState([]);
-
-    const getFeedPost = async () => {
-        const response = await fetch("/api/post",{
-            cache:'no-store',
-            next:{
-                revalidate:1
-            }
-        });
-        const data = await response.json();
-        setFeedPost(data);
-        setLoading(false);
-    };
-
+    // Redirect to sign-in page if the user is not logged in
     useEffect(() => {
-        getFeedPost()
-    }, []);
+        if (isLoaded && !user) {
+            router.push('/sign-in');
+        }
+    }, [isLoaded, user, router]);
 
-    return loading || !isLoaded ? (
-        <Loader />
-    ) : (
+    // Fetch posts data using SWR
+    const { data, error, mutate } = useSWR(isLoaded && user ? '/api/post' : null, fetcher, {
+        shouldRetryOnError: false,
+        refreshInterval: 1000, // Revalidate cache every second
+    });
+
+    // Render loader while user data or posts data is loading
+    if (!isLoaded || !user || !data) {
+        return <Loader />;
+    }
+
+    if (error) {
+        return <div>Failed to load</div>;
+    }
+
+    return (
         <div className="flex flex-col gap-10">
-            {feedPost.map((post) => (
+            {data.map((post) => (
                 <PostCard
                     key={post._id}
                     post={post}
                     creator={post.creator}
                     loggedInUser={user}
-                    update={getFeedPost}
+                    update={mutate} // Pass the mutate function to revalidate data
                 />
             ))}
         </div>
--- app/api/post/route.js
+++ app/api/post/route.js
@@ -1,18 +1,17 @@
 import Post from "@lib/models/Post";
 import { connectToDatabase } from "@lib/mongodb/mongoose";
 
-export const GET = async (res) => {
+export async function GET() {
   try {
     await connectToDatabase();
 
     const feedPosts = await Post.find().populate("creator likes").exec();
 
-
-    return new Response(JSON.stringify(feedPosts), { status: 200 });
+    return new Response(JSON.stringify(feedPosts), {status: 200});
 
 
   } catch (err) {
     console.error("Error fetching feed posts:", err);
-    return new Response("Failed to fetch all Feed Posts", { status: 500 });
+    return new Response("Failed to fetch all Feed Posts", {status: 500});
   }
-};
+}
