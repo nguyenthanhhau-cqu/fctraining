b73207e2cebe3d54d85a942ab7b472f273b5f5ef
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-06-01 01:05:46 +1000

    V3

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,50 +1,44 @@
-'use client';
+"use client";
 
 import { useUser } from "@clerk/nextjs";
 import Loader from "@components/Loader";
 import PostCard from "@components/cards/PostCard";
-import useSWR from 'swr';
-import { useEffect } from "react";
-import { useRouter } from 'next/navigation';
-
-// Fetcher function to fetch data from the API
-const fetcher = url => fetch(url).then(res => res.json());
+import { useEffect, useState } from "react";
 
 const Home = () => {
     const { user, isLoaded } = useUser();
-    const router = useRouter();
 
-    // Redirect to sign-in page if the user is not logged in
+    const [loading, setLoading] = useState(true);
+
+    const [feedPost, setFeedPost] = useState([]);
+
+    const getFeedPost = async () => {
+        const response = await fetch("/api/post",{
+            cache: "no-cache",
+            next:{
+                revalidate: 2
+            }
+        });
+        const data = await response.json();
+        setFeedPost(data);
+        setLoading(false);
+    };
+
     useEffect(() => {
-        if (isLoaded && !user) {
-            router.push('/sign-in');
-        }
-    }, [isLoaded, user, router]);
+        getFeedPost()
+    }, []);
 
-    // Fetch posts data using SWR
-    const { data, error, mutate } = useSWR(isLoaded && user ? '/api/post' : null, fetcher, {
-        shouldRetryOnError: false,
-        refreshInterval: 1000, // Revalidate cache every second
-    });
-
-    // Render loader while user data or posts data is loading
-    if (!isLoaded || !user || !data) {
-        return <Loader />;
-    }
-
-    if (error) {
-        return <div>Failed to load</div>;
-    }
-
-    return (
+    return loading || !isLoaded ? (
+        <Loader />
+    ) : (
         <div className="flex flex-col gap-10">
-            {data.map((post) => (
+            {feedPost.map((post) => (
                 <PostCard
                     key={post._id}
                     post={post}
                     creator={post.creator}
                     loggedInUser={user}
-                    update={mutate} // Pass the mutate function to revalidate data
+                    update={getFeedPost}
                 />
             ))}
         </div>
