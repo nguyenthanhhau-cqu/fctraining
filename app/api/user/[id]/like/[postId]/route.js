8c31c6c67633fd0e8c97643a7f1a929af263bb0d
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-06-01 01:11:01 +1000

    V3

--- app/api/user/[id]/like/[postId]/route.js
+++ app/api/user/[id]/like/[postId]/route.js
@@ -3,39 +3,39 @@ import User from "@lib/models/User"
 import { connectToDatabase } from "@lib/mongodb/mongoose"
 
 export const POST = async (req, { params }) => {
-  try {
-    await connectToDatabase()
+    try {
+        await connectToDatabase()
 
-    const userId = params.id
-    const postId = params.postId
+        const userId = params.id
+        const postId = params.postId
 
-    const user = await User.findOne({ clerkId: userId }).populate("posts savedPosts following followers").populate({
-      path: "likedPosts",
-      model: "Post",
-      populate: {
-        path: "creator",
-        model: "User",
-      },
-    })
+        const user = await User.findOne({ clerkId: userId }).populate("posts savedPosts following followers").populate({
+            path: "likedPosts",
+            model: "Post",
+            populate: {
+                path: "creator",
+                model: "User",
+            },
+        })
 
-    const post = await Post.findById(postId).populate("creator likes")
+        const post = await Post.findById(postId).populate("creator likes")
 
-    const isLiked = user.likedPosts.find((item) => item._id.toString() === postId)
+        const isLiked = user.likedPosts.find((item) => item._id.toString() === postId)
 
-    if (isLiked) {
-      user.likedPosts = user.likedPosts.filter((item) => item._id.toString() !== postId)
-      post.likes = post.likes.filter((item) => item._id.toString() !== user._id.toString())
-    } else {
-      user.likedPosts.push(post._id)
-      post.likes.push(user._id)
+        if (isLiked) {
+            user.likedPosts = user.likedPosts.filter((item) => item._id.toString() !== postId)
+            post.likes = post.likes.filter((item) => item._id.toString() !== user._id.toString())
+        } else {
+            user.likedPosts.push(post._id)
+            post.likes.push(user._id)
+        }
+
+        await user.save()
+        await post.save()
+
+        return new Response(JSON.stringify(user), { status: 200 })
+    } catch (err) {
+        console.log(err)
+        return new Response("Failed to like/dislike post", { status: 500 })
     }
-
-    await user.save()
-    await post.save()
-
-    return new Response(JSON.stringify(user), { status: 200 })
-  } catch (err) {
-    console.log(err)
-    return new Response("Failed to like/dislike post", { status: 500 })
-  }
 }
