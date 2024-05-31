4900a3c1faa75dfe6647481666978c3d52dd53cc
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-05-31 17:58:49 +1000

    V3

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,6 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
- 
+
 export default function Page() {
-  return <SignIn appearance={{ baseTheme: dark }} />;
+  return <SignIn />;
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,6 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
- 
+
 export default function Page() {
-  return <SignUp appearance={{ baseTheme: dark }} />;
+  return <SignUp />;
 } 
