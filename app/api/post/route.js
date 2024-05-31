44abeeded9973e6a7f3eb5ed595028902ffee5a8
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-06-01 01:50:38 +1000

    V3

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -1,13 +1,18 @@
 import Post from "@lib/models/Post";
 import { connectToDatabase } from "@lib/mongodb/mongoose";
 
-export const GET = async (req) => {
+export const GET = async (req,res) => {
   try {
     await connectToDatabase();
 
     const feedPosts = await Post.find().populate("creator likes").exec();
 
+    res.setHeader('Cache-Control', 'public, max-age=60, s-maxage=120');
+
+
     return new Response(JSON.stringify(feedPosts), { status: 200 });
+
+
   } catch (err) {
     console.error("Error fetching feed posts:", err);
     return new Response("Failed to fetch all Feed Posts", { status: 500 });
