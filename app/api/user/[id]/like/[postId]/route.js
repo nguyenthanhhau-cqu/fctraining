ffabe8c575ded45fcf75301d726fa5b565c7cfe8
nguyenthanhhau-cqu <thanhhau.nguyen@cqumail.com>
2024-06-07 13:04:28 +1000

    V3

--- app/api/user/[id]/like/[postId]/route.js
+++ app/api/user/[id]/like/[postId]/route.js
@@ -38,7 +38,6 @@ export const POST = async (req, { params }) => {
             status: 200,
             headers: {
                 'Content-Type': 'application/json',
-                'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate'
             },
         });
     } catch (err) {
